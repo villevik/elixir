2b3ba42a1d45d948a5c8a70ce90b2c94790d510a
villevik <59703108+villevik@users.noreply.github.com>
2022-02-02 00:10:58 +0100

    Better values

--- emulator/program.ex
+++ emulator/program.ex
@@ -6,15 +6,17 @@ defmodule Program do
     ##{List.to_tuple(test())}
   end
   def test() do
-   {  {:addi, 1, 1, 5},
+   {
+      {:addi, 1, 1, 5},
       {:add, 2, 1, 1},
       {:sub, 3, 2, 1},
-      {:addi, 4, 1, 5},
-      {:addi, 4, 4, 10},
+      {:addi, 4, 1, 2},
+      {:addi, 4, 4, 3},
       {:sw, 4, 2, 1},
       {:lw, 5, 2, 1},
       {:out, 3},
       {:beq, 2, 4, -4},
+      {:out, 5},
       {:halt}
   }
   end
