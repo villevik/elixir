3da588733ade8d2816a29d1b17f5bdb5a73838fa
villevik <59703108+villevik@users.noreply.github.com>
2022-03-10 15:33:57 +0100

    pictures

--- mandelbrot/test.ex
+++ mandelbrot/test.ex
@@ -1,14 +1,14 @@
 defmodule Test do
   def demo() do
-    small(-2.6, 1.2, 1.2)
+    small(-0.6, 0.65, -0.4)
   end
   def small(x0, y0, xn) do
     width = 960
     height = 540
-    depth = 64
+    depth = 100
     k = (xn - x0) / width
     image = Mandel.mandelbrot(width, height, x0, y0, k, depth)
-    PPM.write("small2.ppm", image)
+    PPM.write("small5.ppm", image)
   end
 
 end
